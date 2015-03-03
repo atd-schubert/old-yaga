53954bcf94fd1d452a59adf63a7ba797573ad013
Arne Schubert <atd.schubert@gmail.com>
2015-03-03 17:56:21 +0100

    Correct timing and Panel class

--- client/scripts/yaga.js
+++ client/scripts/yaga.js
@@ -3,13 +3,19 @@
 define('yaga', ['jquery', 'jqueryMobile', 'leaflet'], function ($, jqm, L) {
     'use strict';
 
-    var yaga, map, document, Toolbar;
+    var yaga, map, document, Toolbar, Panel;
     document = window.document;
     // Settings
     //$('[data-role="footer"]').toolbar({ tapToggle: false});
     map = L.map('yaga-map');
     $(document).ready(function () {
-        $('[data-role="header"], [data-role="footer"]').toolbar({tapToggle: false});
+
+        new Toolbar('header', '#yaga-header');
+        new Toolbar('footer', '#yaga-footer');
+
+        new Panel('settings', '#settings-panel');
+        new Panel('tools', '#tools-panel');
+
         map.setView([51.505, -0.09], 13);
 
         L.tileLayer('http://a.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
@@ -31,9 +37,28 @@ define('yaga', ['jquery', 'jqueryMobile', 'leaflet'], function ($, jqm, L) {
         yaga.Toolbar.toolsbars.footer.toggle();
     });
 
-    Toolbar = function (name, elem) {
-        Toolbar.toolsbars[name] = this;
+    Panel = function (name, elem) {
+        Panel.panels[name] = this;
         var $elem = $(elem);
+        this.open = function () {
+            $elem.panel('open');
+        };
+        this.close = function () {
+            $elem.panel('close');
+        };
+        this.toggle = function () {
+            $elem.panel('toggle');
+        };
+
+    };
+    Panel.panels = {};
+
+    Toolbar = function (name, elem) {
+        var $elem = $(elem);
+
+        Toolbar.toolsbars[name] = this;
+        $elem.toolbar({tapToggle: false});
+
         this.status = "show";
         this.show = function () {
             $elem.toolbar('show');
@@ -53,13 +78,12 @@ define('yaga', ['jquery', 'jqueryMobile', 'leaflet'], function ($, jqm, L) {
     };
     Toolbar.toolsbars = {};
 
-    new Toolbar('header', '#yaga-header');
-    new Toolbar('footer', '#yaga-footer');
 
     yaga = {
         // Instances:
         map: map,
-        Toolbar: Toolbar
+        Toolbar: Toolbar,
+        Panel: Panel
         // UI-Elements
     };
 
