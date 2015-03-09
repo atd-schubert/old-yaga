b6496fa98679e3f5e7a7f9bf8e048b44980816f6
Arne Schubert <atd.schubert@gmail.com>
2015-03-09 10:12:34 +0100

    adding a gruntfile to make a test task

---
+++ client/Gruntfile.js
@@ -0,0 +1,79 @@
+/*jslint node: true */
+'use strict';
+
+/**
+ * This is the basic Gruntfile for Wheregroup PHP projects.
+ *
+ * @module ./Gruntfile.js
+ * @author Arne Schubert <atd.schubert@gmail.com>
+ */
+
+/**
+ * Common.JS object
+ * @name module
+ * @type {object}
+ * @property {*} exports - Reference to export module data
+ */
+
+/**
+ * Function to require Common.JS modules
+ * @name require
+ * @type {function}
+ */
+
+/**
+ * Grunt module
+ * @name grunt
+ * @link http://gruntjs.com/api/grunt - Grunt API Documentation
+ * @requires grunt
+ * @property {function} initConfig
+ * @property {function} loadNpmTasks
+ * @property {function} registerMultiTask
+ * @property {function} registerTask
+ * @property {object} config
+ * @property {object} task
+ * @property {function} task.run
+ * @property {object} log
+ * @property {object} file
+ * @property {object} fail
+ * @property {function} fail.fatal
+ * @type {object}
+ */
+
+/**
+ * Task-runner for wheregroup php projects
+ * @param {grunt} grunt
+ */
+module.exports = function wheregroupTaskRunner(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        connect: {
+            client: {
+                options: {
+                    keepalive: true,
+                    port: 3000,
+                    hostname: '*',
+                    base: {
+                        path: './',
+                        options: {
+                            index: 'index.html',
+                            maxAge: 300000
+                        }
+                    }
+                }
+            }
+        }
+    });
+
+
+// Loaded-tasks:
+// General
+    /**
+     * Task for opening static files in a virtual server
+     * @link https://github.com/gruntjs/grunt-contrib-connect - Github
+     * @requires grunt-contrib-connect/tasks/connect.js
+     */
+    grunt.loadNpmTasks('grunt-contrib-connect');
+
+    grunt.registerTask('default', ['connect:client']);
+};
